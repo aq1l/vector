dad51cdd4848c3a0a3ea9c6c411dd2241310241f
Lukasz Drozdz <lukasz.drozdz93@gmail.com>
2024-05-16 11:13:34 +0200

    add azure source metadata definition

--- src/sources/azure_container/mod.rs
+++ src/sources/azure_container/mod.rs
@@ -2,8 +2,12 @@ use std::time::Duration;
 use futures_util::StreamExt;
 use tokio::time;
 use tokio_stream::wrappers::IntervalStream;
+use vrl::{owned_value_path, path};
+use vrl::prelude::Kind;
+use vector_lib::codecs::SyslogDeserializerConfig;
+use vector_lib::config::LegacyKey;
 use vector_lib::event::{EventMetadata, LogEvent};
-use crate::config::{LogNamespace, SourceConfig, SourceContext, SourceOutput};
+use crate::config::{DataType, LogNamespace, SourceConfig, SourceContext, SourceOutput};
 use crate::event::Event;
 use crate::shutdown::ShutdownSignal;
 use crate::sinks::prelude::configurable_component;
@@ -59,8 +63,20 @@ impl SourceConfig for AzureContainerConfig {
         )))
     }
 
-    fn outputs(&self, _: LogNamespace) -> Vec<SourceOutput> {
-        unimplemented!()
+    fn outputs(&self, global_log_namespace: LogNamespace) -> Vec<SourceOutput> {
+        let log_namespace = global_log_namespace.merge(self.log_namespace);
+        let schema_definition = SyslogDeserializerConfig::from_source(Self::NAME)
+            .schema_definition(log_namespace)
+            .with_standard_vector_source_metadata()
+            .with_source_metadata(
+                Self::NAME,
+                Some(LegacyKey::Overwrite(owned_value_path!("some_additional_metadata"))),
+                &owned_value_path!("some_additional_metadata"),
+                Kind::bytes(),
+                None,
+            );
+
+        vec![SourceOutput::new_logs(DataType::Log, schema_definition)]
     }
 
     fn can_acknowledge(&self) -> bool {
@@ -75,7 +91,7 @@ async fn run_scheduled(
     exec_interval_secs: u64,
     shutdown: ShutdownSignal,
     out: SourceSender,
-    _: LogNamespace,
+    log_namespace: LogNamespace,
 ) -> Result<(), ()> {
     debug!("Starting scheduled exec runs.");
     let schedule = Duration::from_secs(exec_interval_secs);
@@ -85,10 +101,18 @@ async fn run_scheduled(
     while interval.next().await.is_some() {
         counter += 1;
 
-        let log_event = LogEvent::from_map(btreemap! {
+        let mut log_event = LogEvent::from_map(btreemap! {
             "message" => counter.to_string(),
         }, EventMetadata::default().into());
+        log_namespace.insert_source_metadata(
+            AzureContainerConfig::NAME,
+            &mut log_event,
+            Some(LegacyKey::Overwrite("some_additional_metadata")),
+            path!("some_additional_metadata"),
+            "some content".to_owned(),
+        );
         let event = Event::Log(log_event);
+
         out.clone().send_event(event).await.unwrap();
     }
     Ok(())
